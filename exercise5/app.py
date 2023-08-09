36a238e7e63ac0908cea0dfaf996b86eb1dc7767
Ubuntu <earl@dev2.4pyspfa2jwaelhpu3c5vaewa0d.zx.internal.cloudapp.net>
2023-08-09 09:52:17 +0000

    EG: exercise 5 added (validators)

---
+++ exercise5/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='SOME_KEY'
+
+class UserCheck:
+    def __init__(self, banned, message=None):
+        self.banned = banned
+        if not message:
+            message = 'Please choose another username'
+        self.message = message
+
+    def __call__(self, form, field):
+        if field.data.lower() in (word.lower() for word in self.banned):
+            raise ValidationError(self.message)
+        
+class CharCheck:
+    def __init__(self, banned, message=None):
+        self.banned = banned
+        if not message:
+            message = 'Please choose another username'
+        self.message = message
+
+    def __call__(self, form, field):
+        for item in self.banned:
+            if item in field.data.lower():
+                raise ValidationError(self.message)
+
+
+
+class myForm(FlaskForm):
+    username = StringField('Username', validators=[
+        DataRequired(),
+        UserCheck(message="That username is not allowed", banned = ['root','admin','sys']),
+        Length(min=2,max=15),
+        CharCheck(message="Cannot have special characters!", banned = ['!', '"', "'", '@', '#', '$', '*'])
+        ])
+    submit = SubmitField('Sign up')
+
+@app.route('/', methods=['GET','POST'])
+def postName():
+    form = myForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        return render_template('home.html', form = form, username=username)
+    else:
+        return render_template('home.html', form = form, username="")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
